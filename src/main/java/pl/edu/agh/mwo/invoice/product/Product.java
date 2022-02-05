ccf5fbedfea7e3c14ea7132548cdfdefa8cadb84
Regis <pjfw78@gmail.com>
2022-02-05 14:09:07 +0100

    test 5 working

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,8 +10,8 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
-        if( name.isEmpty()){
-            throw new IllegalArgumentException();
+        if( name == null){
+            throw new IllegalArgumentException("Product name cannot be null");
         }
         this.name = name;
         this.price = price;
